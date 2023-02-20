b1433a514636ce5ff35503ba448e59e1f6d004e3
Kathryn <kathrynlytle@hotmail.com>
2023-02-20 12:19:32 -0600

    m

--- user.js
+++ user.js
@@ -1,16 +1,28 @@
-const postListEl = document.querySelector('.post-list')
+const postListEl = document.querySelector('.post-list');
 
-function onSearchChange() {
-    console.log('1234')
+async function onSearchChange(event) {
+    const id = event.target.value; 
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postsData = await posts.json(); 
+    postListEl.innerHTML = postsData.map(post => `
+        <div class="post">
+            <div class="post__title">
+                ${post.title}
+            </div>
+            <p class="post__body">
+             ${post.body}
+             </p>
+         </div>
+`).join('');
 }
 
-async function main() {
-    const id = localStorage.getItem("id")
+async function render() {
+    const id = localStorage.getItem("id"); 
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json(); 
     console.log(postsData)
 
-   postListEl.innerHTML = postsData.map(post => `
+    postListEl.innerHTML = postsData.map(post => `
     <div class="post">
       <div class="post__title">
         ${post.title}
@@ -19,7 +31,8 @@ async function main() {
         ${post.body}
       </p>
     </div>
-`).join('')
-}
+`).join('');
+
+}  
 
 main(); 
